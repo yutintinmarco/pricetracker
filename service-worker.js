961cac22f5a2e448be9012cdca86b034939e1506
yutintinmarco <marcoyu1121@gmail.com>
2026-08-20 16:55:57 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "barcode-price-tracker-v125-offer-validation-guard";
-const ASSET_VERSION = "v125";
+const CACHE_NAME = "barcode-price-tracker-v126-product-price-range";
+const ASSET_VERSION = "v126";
 
 const APP_SHELL = [
   "./",
